406258db4252add6c6bf6c044781e1233823cddc
tony <tonyschneider112@hotmail.com>
2019-03-23 22:45:52 +0200

    first

---
+++ src/Automation_Yad2.java
@@ -0,0 +1,25 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+public class Automation_Yad2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:/Users/tony/Desktop/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		while(true){
+			driver.get("https://my.yad2.co.il/login.php");
+			driver.manage().window().maximize();
+			driver.findElement(By.xpath("//*[@id='userName']")).sendKeys("tonyschneider112@hotmail.com");
+			driver.findElement(By.xpath("//*[@id='password']")).sendKeys("altar112");
+			driver.findElement(By.xpath("//*[@id='submitLogonForm']")).click();
+			driver.findElement(By.xpath("/html/body/div[7]/div/div[3]/div[1]/a")).click();
+			driver.findElement(By.xpath("//*[@id='feed']/tbody/tr[2]")).click();
+			Thread.sleep(3000);
+			driver.switchTo().frame(driver.findElement(By.xpath("//*[@id='feed']/tbody/tr[3]/td/iframe")));
+			driver.findElement(By.xpath("//*[@id='bounceRatingOrderBtn']")).click();
+			Thread.sleep(14400000);
+		}
+	}
+
+}
