27d7faf31c0d726c38d721fad8756d87d12cd339
Tony <tonyschneider112@hotmail.com>
2019-03-23 23:25:23 +0200

    second

--- src/Automation_Yad2.java
+++ src/Automation_Yad2.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.chrome.*;
 public class Automation_Yad2 {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver","C:/Users/tony/Desktop/chromedriver_win32/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:/Users/Tony/Desktop/chromedriver_win32/chromedriver.exe");
+		//C:\Users\Tony\Desktop\chromedriver_win32
 		WebDriver driver = new ChromeDriver();
 		while(true){
 			driver.get("https://my.yad2.co.il/login.php");
@@ -13,7 +14,9 @@ public class Automation_Yad2 {
 			driver.findElement(By.xpath("//*[@id='userName']")).sendKeys("tonyschneider112@hotmail.com");
 			driver.findElement(By.xpath("//*[@id='password']")).sendKeys("altar112");
 			driver.findElement(By.xpath("//*[@id='submitLogonForm']")).click();
-			driver.findElement(By.xpath("/html/body/div[7]/div/div[3]/div[1]/a")).click();
+//			Thread.sleep(3000);
+			driver.findElement(By.xpath("/html/body/div[5]/div/div[3]/div[1]")).click();
+//			Thread.sleep(3000);
 			driver.findElement(By.xpath("//*[@id='feed']/tbody/tr[2]")).click();
 			Thread.sleep(3000);
 			driver.switchTo().frame(driver.findElement(By.xpath("//*[@id='feed']/tbody/tr[3]/td/iframe")));
